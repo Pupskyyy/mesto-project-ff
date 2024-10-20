e018f2d12e3c6f6b0e6efc0aa7ccd6347f56200a
Anton Karavannov <a.karavannov@yandex.ru>
2024-10-21 01:53:58 +0400

    добавление карточек и их удаление с помощью js

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,18 @@
 const cardList = document.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
 
-function createCard(element) {
+function createCard (initialCards, deleteCard){
+	initialCards.forEach(function (element){
   const card = cardTemplate.querySelector(".places__item").cloneNode(true);
   const deleteButton = card.querySelector(".card__delete-button");
   card.querySelector(".card__image").src = element.link;
   card.querySelector(".card__title").textContent = element.name;
   cardList.append(card);
-  deleteButton.addEventListener("click", deleteCard);
+  deleteButton.addEventListener("click", deleteCard);});
 }
 
 function deleteCard(item) {
   item.target.parentElement.remove();
 }
 
-initialCards.forEach(createCard);
+createCard (initialCards, deleteCard);
