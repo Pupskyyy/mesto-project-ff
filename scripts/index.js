f3f7214793f73bfe65b07d78c39e3bf467a7f85a
Anton Karavannov <a.karavannov@yandex.ru>
2024-10-21 22:28:11 +0400

    убрал ненужную запятую

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const cardList = document.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
 
-function createCard (imageSource, textDescription, deleteCard){
+function createCard(imageSource, textDescription, deleteCard) {
   const card = cardTemplate.querySelector(".places__item").cloneNode(true);
   const deleteButton = card.querySelector(".card__delete-button");
   card.querySelector(".card__image").src = imageSource;
@@ -15,10 +15,10 @@ function deleteCard(evt) {
   evt.target.closest(".places__item").remove();
 }
 
-function renderCard(imageSource, textDescription,) {
-  cardList.append(createCard (imageSource, textDescription, deleteCard));
+function renderCard(imageSource, textDescription) {
+  cardList.append(createCard(imageSource, textDescription, deleteCard));
 }
 
-initialCards.forEach(function (element){
+initialCards.forEach(function (element) {
   renderCard(element.link, element.name);
 });
