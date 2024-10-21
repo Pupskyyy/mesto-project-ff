0caa12fda01ea013ccdd55e3e317927bb67d1a01
Anton Karavannov <a.karavannov@yandex.ru>
2024-10-21 22:20:13 +0400

    Изменение логики создания и добавления карточек. Функция удаления карточки исправлена

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,24 @@
 const cardList = document.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
 
-function createCards (initialCards, deleteCard){
-	initialCards.forEach(function (element){
+function createCard (imageSource, textDescription, deleteCard){
   const card = cardTemplate.querySelector(".places__item").cloneNode(true);
   const deleteButton = card.querySelector(".card__delete-button");
-  card.querySelector(".card__image").src = element.link;
-  card.querySelector(".card__title").textContent = element.name;
-  cardList.append(card);
-  deleteButton.addEventListener("click", deleteCard);});
+  card.querySelector(".card__image").src = imageSource;
+  card.querySelector(".card__image").alt = `Фотография: ${textDescription}`;
+  card.querySelector(".card__title").textContent = textDescription;
+  deleteButton.addEventListener("click", deleteCard);
+  return card;
 }
 
-function deleteCard(item) {
-  item.target.parentElement.remove();
+function deleteCard(evt) {
+  evt.target.closest(".places__item").remove();
 }
 
-createCards (initialCards, deleteCard);
+function renderCard(imageSource, textDescription,) {
+  cardList.append(createCard (imageSource, textDescription, deleteCard));
+}
+
+initialCards.forEach(function (element){
+  renderCard(element.link, element.name);
+});
